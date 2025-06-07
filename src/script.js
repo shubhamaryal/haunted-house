7655f3e0f7fe3127175f4c3ccbb3d0e302926419
shubhamaryal <aryalshubham62@gmail.com>
2025-06-07 13:52:43 +0545

    added shadows and optimized it

--- src/script.js
+++ src/script.js
@@ -169,6 +169,9 @@ for (let i = 0; i < 50; i++) {
   grave.position.set(x, 0.3, z);
   grave.rotation.y = (Math.random() - 0.5) * 0.4;
   grave.rotation.z = (Math.random() - 0.5) * 0.4;
+
+  grave.castShadow = true; 
+
   graves.add(grave);
 }
 
@@ -270,7 +273,44 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setClearColor("#262837"); // set the color of the renderer to the same as the fog color
+
+/**
+ * Shadows
+ */
 renderer.shadowMap.enabled = true; 
+
+moonLight.castShadow = true; 
+doorLight.castShadow = true; 
+ghost1.castShadow = true; 
+ghost2.castShadow = true; 
+ghost3.castShadow = true; 
+
+walls.castShadow = true;
+bush1.castShadow = true;
+bush2.castShadow = true;
+bush3.castShadow = true;
+bush4.castShadow = true;
+
+floor.receiveShadow = true;
+
+doorLight.shadow.mapSize.width = 256; 
+doorLight.shadow.mapSize.height = 256; 
+doorLight.shadow.camera.far = 7;
+
+ghost1.shadow.mapSize.width = 256;
+ghost1.shadow.mapSize.height = 256;
+ghost1.shadow.camera.far = 7;
+
+ghost2.shadow.mapSize.width = 256;
+ghost2.shadow.mapSize.height = 256;
+ghost2.shadow.camera.far = 7;
+
+ghost3.shadow.mapSize.width = 256;
+ghost3.shadow.mapSize.height = 256;
+ghost3.shadow.camera.far = 7;
+
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
+
 
 /**
  * Animate
